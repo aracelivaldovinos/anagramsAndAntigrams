bea8e50c3b3afe9f8d9328994ef628bc4fee8a20
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-07-16 11:50:50 -0700

    WOP: account for two sentences being compared as anagrams or antigrams.

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,13 +1,14 @@
 class Word
   def initialize(word1)
-    @array1 = word1.split("")
+    @array1 = word1.gsub(/[^a-zA-Z0-9\-]/,"").split(" ")
   end 
 
   def anagrams_antigrams(word2)
+    new_word2 = word2.gsub(/[^a-zA-Z0-9\-]/,"")
     @array1.each do |element|
-      if word2.include?(element)&& word2.match?(/[aeiou]/i)
+      if word2.match?(element)&& word2.match?(/[aeiou]/i) 
         return ("These words are anagrams.")
-      elsif (word2).include?element 
+      elsif (word2).match?element 
         return ("You need to input actual words!") 
       elsif word2.match?(/[aeiou]/i) && element.match?(/[aeiou]/i)
         return ("These words have no letter matches and are antigrams.")
