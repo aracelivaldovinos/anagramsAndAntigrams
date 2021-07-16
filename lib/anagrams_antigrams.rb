1c64d3cae2bcdf971adace9f8b478bfbb8524555
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-07-16 12:49:53 -0700

    it remove special character and check if two words are anagrams

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,19 +1,34 @@
 class Word
-  def initialize(word1)
-    @array1 = word1.gsub(/[^a-zA-Z0-9\-]/,"").split(" ")
+  def initialize(str1)
+    @user_input = str1.gsub(/[^a-zA-Z0-9\-]/,"")
   end 
-
-  def anagrams_antigrams(word2)
-    new_word2 = word2.gsub(/[^a-zA-Z0-9\-]/,"")
-    @array1.each do |element|
-      if word2.match?(element)&& word2.match?(/[aeiou]/i) 
-        return ("These words are anagrams.")
-      elsif (word2).match?element 
-        return ("You need to input actual words!") 
-      elsif word2.match?(/[aeiou]/i) && element.match?(/[aeiou]/i)
-        return ("These words have no letter matches and are antigrams.")
-      end 
-    end 
-  end
+  def anagrams_antigrams(str2)
+    comparing_string = str2.gsub(/[^a-zA-Z0-9\-]/,"")
+     if @user_input.chars.sort == comparing_string.chars.sort
+      return ("These words are anagrams.")
+     else
+      return ("These words are not anagrams.")
+     end
+  end 
 end 
+
+
+# class Word
+#   def initialize(word1)
+#     @array1 = word1.gsub(/[^a-zA-Z0-9\-]/,"").split(" ")
+#   end 
+
+#   def anagrams_antigrams(word2)
+#     new_word2 = word2.gsub(/[^a-zA-Z0-9\-]/,"")
+#     @array1.each do |element|
+#       if word2.match?(element)&& word2.match?(/[aeiou]/i) 
+#         return ("These words are anagrams.")
+#       elsif (word2).match?element 
+#         return ("You need to input actual words!") 
+#       elsif word2.match?(/[aeiou]/i) && element.match?(/[aeiou]/i)
+#         return ("These words have no letter matches and are antigrams.")
+#       end 
+#     end 
+#   end
+# end 
 
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -3,21 +3,21 @@ require('anagrams_antigrams')
 require('pry')
 
 describe('#anagrams_antigrams') do
-  it("Check if two words are anagrams") do
-    anagram1 = Word.new("ruby")
-    expect(anagram1.anagrams_antigrams("bury")).to(eq("These words are anagrams."))
-  end
-  it("It will account for the possibility that words might have different cases but should still be anagrams") do
-    anagram2 = Word.new("tea")
-    expect(anagram2.anagrams_antigrams("eat")).to(eq("These words are anagrams."))
-  end
-  it("Check if the inputs are words") do
-    anagram3 = Word.new("kmpn")
-    expect(anagram3.anagrams_antigrams("pkmn")).to(eq("You need to input actual words!"))
-  end
-  it("Check whether they are actually antigrams.") do
-    anagram3 = Word.new("hi")
-    expect(anagram3.anagrams_antigrams("bye")).to(eq("These words have no letter matches and are antigrams."))
-  end
+    it("Check if two words are anagrams") do
+      anagram1 = Word.new("ruby!")
+      expect(anagram1.anagrams_antigrams("bury.")).to(eq("These words are anagrams."))
+    end
+  # it("It will account for the possibility that words might have different cases but should still be anagrams") do
+  #   anagram2 = Word.new("tea")
+  #   expect(anagram2.anagrams_antigrams("eat")).to(eq("These words are anagrams."))
+  # end
+  # it("Check if the inputs are words") do
+  #   anagram3 = Word.new("kmpn")
+  #   expect(anagram3.anagrams_antigrams("pkmn")).to(eq("You need to input actual words!"))
+  # end
+  # it("Check whether they are actually antigrams.") do
+  #   anagram3 = Word.new("hi")
+  #   expect(anagram3.anagrams_antigrams("bye")).to(eq("These words have no letter matches and are antigrams."))
+  # end
  
 end
